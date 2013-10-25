7838b657c608850698f5e1302dd88d3f8ca1f98b
Daniel <daniel@winterwell.com>
2013-10-25 14:26:46 +0100

    sync

--- build.sh
+++ build.sh
@@ -5,5 +5,11 @@ echo "This script assumes you have a winterwell setup!"
 jsdoc SJTest.js
 yuicompressor -o SJTest.min.js SJTest.js
 cp SJTest.min.js ~/winterwell/code/creole/web/static/code/lib/
+
 rm -rf ~/winterwell/www/software/sjtest/out
 cp -R out ~/winterwell/www/software/sjtest/out
+
+rm -rf ~/winterwell/www/software/sjtest/test
+cp -R test ~/winterwell/www/software/sjtest/test
+
+echo "Done :)"
