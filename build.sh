2ab7f249b7641c42e7cc7147579933cf071f0606
Daniel Winterstein <daniel.winterstein@gmail.com>
2015-02-25 02:29:45 +0000

    wip

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ echo "This script assumes you have a winterwell setup!"
 
 jsdoc SJTest.js
 yuicompressor -o SJTest.min.js SJTest.js
-cp SJTest.js ~/winterwell/code/creole/web/static/code/lib/
-cp SJTest.min.js ~/winterwell/code/creole/web/static/code/lib/
+cp SJTest.js ~/winterwell/sodash/web/static/code/lib/
+cp SJTest.min.js ~/winterwell/sodash/web/static/code/lib/
 
 rm -rf ~/winterwell/www/software/sjtest/out
 cp -R out ~/winterwell/www/software/sjtest/out
